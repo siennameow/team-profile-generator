b9ad328721359a0e13371f3474ab733ec63383b6
Hexuan Li <lihexuan1@gmail.com>
2022-04-23 11:08:12 -0700

    add prompt questions for manager, engineer and intern

--- index.js
+++ index.js
@@ -42,6 +42,9 @@ const addManager = () => {
             name: 'officeNumber',
             message: 'What is the team manager\'s office number?',
         },
+        //WHEN I enter the team manager’s name, employee ID, email address, and office number
+        //THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+        //function to add an employee
         {
             type: 'list',
             name: 'addMember',
@@ -50,18 +53,73 @@ const addManager = () => {
         }
     ])
 }
-//WHEN I enter the team manager’s name, employee ID, email address, and office number
-//THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-//function to add an employee
 
 //WHEN I select the engineer option
 //THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
 // function to add engineer prompt
-
+const addEngineer = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the engineer\'s name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the engineer\'s id?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the engineer\'s email address?',
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'What is the engineer\'s GitHub username?',
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'What type of team member would you like to add next?',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
+        }
+    ])
+}
 //WHEN I select the intern option
 //THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
 // function to add intern prompt
-
+const addIntern = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the intern\'s name?'
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the intern\'s id?'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the intern\'s email address?'
+        },
+        {
+            type: 'input',
+            name: 'school',
+            message: 'What is the intern\'s school?'
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'What type of team member would you like to add next?',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
+        }
+    ])
+}
 //WHEN I am prompted for my team members and their information
 //THEN an HTML file is generated that displays a nicely formatted team roster based on user input
 //function to generate HTML page file 
