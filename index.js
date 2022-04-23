2200f8b17ea3cc9987df234bdc6e47e7a5dd4faf
Hexuan Li <lihexuan1@gmail.com>
2022-04-23 11:02:34 -0700

    create guildline for index.js, import node modules

--- index.js
+++ index.js
@@ -1,26 +1,75 @@
-//User Story
-//AS A manager
-//I WANT to generate a webpage that displays my team's basic info
-//SO THAT I have quick access to their emails and GitHub profiles
 
-//Acceptance Criteria
+// import bnode modules 
+const inquirer = require("inquirer");
+const fs = require("fs");
+//import classes
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const Manager = require('./lib/Manager');
 
-//GIVEN a command-line application that accepts user input
-//WHEN I am prompted for my team members and their information
-//THEN an HTML file is generated that displays a nicely formatted team roster based on user input
-//WHEN I click on an email address in the HTML
-//THEN my default email program opens and populates the TO field of the email with the address
-//WHEN I click on the GitHub username
-//THEN that GitHub profile opens in a new tab
 //WHEN I start the application
 //THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
+//function to start of manager prompts
+const addManager = () => {
+  inquirer.prompt([
+        {
+        name: 'role',
+        type: 'confirm',
+        message: `
+        =================
+        Welcome to the Team Profile Generator! Are you ready to begin?
+        =================
+        `
+        },
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the team manager\'s name?',
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: 'What is the team manager\'s id?',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'What is the team manager\'s email?',
+        },
+        {
+            type: 'input',
+            name: 'officeNumber',
+            message: 'What is the team manager\'s office number?',
+        },
+        {
+            type: 'list',
+            name: 'addMember',
+            message: 'What type of team member would you like to add?',
+            choices: ['Engineer', 'Intern', 'I don\'t want to add any more team members'],
+        }
+    ])
+}
 //WHEN I enter the team manager’s name, employee ID, email address, and office number
 //THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+//function to add an employee
+
 //WHEN I select the engineer option
 //THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+// function to add engineer prompt
+
 //WHEN I select the intern option
 //THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-//WHEN I decide to finish building my team
-//THEN I exit the application, and the HTML is generated
+// function to add intern prompt
+
+//WHEN I am prompted for my team members and their information
+//THEN an HTML file is generated that displays a nicely formatted team roster based on user input
+//function to generate HTML page file 
+
+//WHEN I click on an email address in the HTML
+//THEN my default email program opens and populates the TO field of the email with the address
+
+//WHEN I click on the GitHub username
+//THEN that GitHub profile opens in a new tab
 
 //Finally, although it’s not a requirement, consider adding validation to ensure that user input is in the proper format.
