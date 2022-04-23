656ed1e07774cad87b0550d1baa591506499f6e4
Hexuan Li <lihexuan1@gmail.com>
2022-04-22 20:11:44 -0700

    add engineer test

--- test/Engineer.test.js
+++ test/Engineer.test.js
@@ -7,39 +7,58 @@ describe("Engineer", () => {
   
     describe("Engineer object instantiation", () => {
       it("Creates an object with name, id, email and github properties", () => {
+        const engineer = new Engineer("Apple Sweet", 1, "applesweet@gmail.com", "applesosweet");
+//same as employee
+        expect(engineer).toBeInstanceOf(Engineer);
+        expect(engineer.name).toEqual("Apple Sweet");
+        expect(engineer.id).toEqual(1);
+        expect(engineer.email).toEqual("applesweet@gmail.com");
+        //add a github check
+        expect(engineer.github).toEqual("applesosweet");
       });
     });
   
-    
+//same as employee
     describe("getName", () => {
       it("Should return the object's name property", () => {
-  
+        const engineer = new Engineer("Apple Sweet", 1, "applesweet@gmail.com", "applesosweet");
+
+        expect(engineer.getName()).toEqual("Apple Sweet");
       });
     });
   
    
     describe("getId", () => {
       it("Should return the object's id property", () => {
-        
+        const engineer = new Engineer("Apple Sweet", 1, "applesweet@gmail.com", "applesosweet");
+
+        expect(engineer.getID()).toEqual(1);
       });
     });
   
   
     describe("getEmail", () => {
       it("Should return the object's email property", () => {
-        
+        const engineer = new Engineer("Apple Sweet", 1, "applesweet@gmail.com", "applesosweet");
+
+        expect(engineer.getEmail()).toEqual("applesweet@gmail.com");
       });
     });
-  
+
+//add getGithub function test
     describe("getGithub", () => {
         it("Should return the object's github property", () => {
+          const engineer = new Engineer("Apple Sweet", 1, "applesweet@gmail.com", "applesosweet");
 
+          expect(engineer.getGithub()).toEqual("applesosweet");
         });
       });
 
     describe("getRole", () => {
       it("Should return the string 'Engineer'", () => {
-        
+        const engineer = new Engineer("Apple Sweet", 1, "applesweet@gmail.com", "applesosweet");
+
+        expect(engineer.getRole()).toEqual("Engineer");
       });
     });
   });
