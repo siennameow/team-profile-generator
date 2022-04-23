fceff2d2b9bae71e0e4cadf6aba3a7da0fc53a5f
Hexuan Li <lihexuan1@gmail.com>
2022-04-22 20:01:27 -0700

    add employee test

--- test/Employee.test.js
+++ test/Employee.test.js
@@ -5,6 +5,12 @@ describe("Employee", () => {
   
   describe("Employee object instantiation", () => {
     it("Creates an object with name, id, and email properties", () => {
+      const employee = new Employee("Apple Sweet", 1, "applesweet@gmail.com");
+
+      expect(employee).toBeInstanceOf(Employee);
+      expect(employee.name).toEqual("Apple Sweet");
+      expect(employee.id).toEqual(1);
+      expect(employee.email).toEqual("applesweet@gmail.com");
     });
   });
 
@@ -12,27 +18,36 @@ describe("Employee", () => {
   describe("getName", () => {
     it("Should return the object's name property", () => {
 
+      const employee = new Employee("Apple Sweet", 1, "applesweet@gmail.com");
+
+      expect(employee.getName()).toEqual("Apple Sweet");
     });
   });
 
  
   describe("getId", () => {
     it("Should return the object's id property", () => {
-      
+      const employee = new Employee("Apple Sweet", 1, "applesweet@gmail.com");
+
+      expect(employee.getID()).toEqual(1);
     });
   });
 
 
   describe("getEmail", () => {
     it("Should return the object's email property", () => {
-      
+      const employee = new Employee("Apple Sweet", 1, "applesweet@gmail.com");
+
+      expect(employee.getEmail()).toEqual("applesweet@gmail.com");
     });
   });
 
 
   describe("getRole", () => {
     it("Should return the string 'Employee'", () => {
-      
+      const employee = new Employee("Apple Sweet", 1, "applesweet@gmail.com");
+
+      expect(employee.getRole()).toEqual("Employee");
     });
   });
 });
